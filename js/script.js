9f86fc4ac4d621a5ec738a3a657eb278e31de0f0
Sergey Timchenko <sergeytimchenko96@gmail.com>
2016-08-16 15:53:55 +0300

    Complete markup and styles for sidebar.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ $(function() {
 		interval: 4000
 	});
 });
+
+$(window).load(function(){
+	var carouselCaptionWidth = $("#sidebar-carousel .active img")
+	.width();
+	$("#sidebar-carousel img").each(function(){
+		$(this).attr("width", carouselCaptionWidth);
+	});
+	$("#sidebar-carousel .sidebar-carousel-caption, \
+	   #sidebar-carousel .carousel-indicators, \
+	   .sidebar .banner")
+	.css("max-width", carouselCaptionWidth + "px");
+});
