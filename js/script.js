d8fa2587beec11c646e973f168c341ec2861fe5a
Sergey Timchenko <art-street93@mail.ru>
2016-08-20 01:35:43 +0300

    Commit to display site.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('.carousel').carousel({
+		interval: 4000
+	});
+
+	$("#elastislide").elastislide();
+});
+
+$(window).load(function(){
+	var carouselCaptionWidth = $("#sidebar-carousel .active img")
+	.width();
+	$("#sidebar-carousel img").each(function(){
+		$(this).attr("width", carouselCaptionWidth);
+	});
+	$("#sidebar-carousel .sidebar-carousel-caption, \
+	   #sidebar-carousel .carousel-indicators, \
+	   .sidebar .banner")
+	.css("max-width", carouselCaptionWidth + "px");
+});
